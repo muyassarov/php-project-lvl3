a625fa4b10b7967b719b32e5601f15b5aeb3d977
Behruz Muyassarov <muyassarov@gmail.com>
2020-08-11 00:56:08 +0500

    Changes database settings to connect to Heroku Db

--- config/database.php
+++ config/database.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Str;
+$database_url = parse_url(getenv("DATABASE_URL"));
 
 return [
 
@@ -66,11 +67,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $database_url['host'],
+            'port' => $database_url['port'],
+            'database' => ltrim($database_url['path'], '/'),
+            'username' => $database_url['user'],
+            'password' => $database_url['pass'],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
