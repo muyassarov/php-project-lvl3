5679b9adbe93a9e25c9b76939e38ce3dc2c763ab
Behruz Muyassarov <muyassarov@gmail.com>
2020-09-28 01:05:37 +0500

    Changes in tests, add ability to test domain check functionality

--- tests/Feature/DomainCheckTest.php
+++ tests/Feature/DomainCheckTest.php
@@ -3,8 +3,11 @@
 namespace Tests\Feature;
 
 use Carbon\Carbon;
+use GuzzleHttp\MessageFormatter;
+use GuzzleHttp\Psr7\Response as Psr7Response;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
 use Tests\TestCase;
@@ -26,13 +29,40 @@ class DomainCheckTest extends TestCase
             'created_at' => Carbon::now(),
         ]);
 
-        Http::fake();
+        $statusCode  = 200;
+        $keywords    = 'key,key1,key2,key3';
+        $description = 'website description';
+        $h1          = 'Hello, world!';
+        $htmlBody    = <<<EOF
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="keywords" content="{$keywords}">
+    <meta name="description" content="{$description}">
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <h1>{$h1}</h1>
+  </body>
+</html>
+EOF;
+
+        Http::fake([
+            // Stub a http response for all endpoints...
+            '*' => Http::response($htmlBody, 200),
+        ]);
 
         $response = $this->post(route('domains.checks', [$id]));
         $response->assertSessionHasNoErrors();
         $response->assertRedirect();
         $this->assertDatabaseHas('domain_checks', [
-            'domain_id' => $id,
+            'domain_id'   => $id,
+            'h1'          => $h1,
+            'keywords'    => $keywords,
+            'description' => $description,
+            'status_code' => $statusCode
         ]);
     }
 }
